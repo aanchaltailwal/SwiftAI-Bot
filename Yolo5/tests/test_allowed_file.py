6f4312ab9d464214f72dac0535f6ac7304aca573
Abhishek Chamoli <AbhishekChamoli73@gmail.com>
2023-07-12 23:21:36 +0530

    Scaling

--- Yolo5/tests/test_allowed_file.py
+++ Yolo5/tests/test_allowed_file.py
@@ -5,9 +5,8 @@ from app import allowed_file, ALLOWED_EXTENSIONS
 class TestBacklogPerInstanceMetric(unittest.TestCase):
     def test_valid_filename(self):
         for ext in ALLOWED_EXTENSIONS:
-            self.assertTrue(allowed_file(f'test.{ext}'))
+            self.assertTrue(allowed_file(f"test.{ext}"))
 
     def test_invalid_filename(self):
-        self.assertFalse(allowed_file(f'file'))
-        self.assertFalse(allowed_file(f'file.csv'))
-
+        self.assertFalse(allowed_file(f"file"))
+        self.assertFalse(allowed_file(f"file.csv"))
